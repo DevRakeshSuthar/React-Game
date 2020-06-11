1b33fac9747e73e73546d25538391a0e3368b835
DevRakeshSuthar <dev.rakeshsuthar@gmail.com>
2020-06-11 11:54:43 +0530

    Completed

---
+++ client/src/Progess.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Progess = () => {
+  return (
+    <div>
+      <h3>{attempt}</h3>
+      <ul>
+        <li>{guesslist}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Progress;
